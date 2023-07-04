d199a8f425c192f0ccee88c7a9177469f9b342bc
ismoiloff <amir.ismoiloff@gmail.com>
2023-07-04 20:22:11 +0300

    added table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -32,11 +32,33 @@ class Dom {
   }
 
   on(eventType, callback) {
-    this.$el.removeEventListener(eventType, callback);
+    this.$el.addEventListener(eventType, callback);
   }
 
   off(eventType, callback) {
-    this.$el.addEventListener(eventType, callback);
+    this.$el.removeEventListener(eventType, callback);
+  }
+
+  get data() {
+    return this.$el.dataset;
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector));
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect();
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector);
+  }
+
+  css(styles = {}) {
+    Object.entries(styles).forEach(([key, value]) => {
+      this.$el.style[key] = value;
+    });
   }
 }
 
